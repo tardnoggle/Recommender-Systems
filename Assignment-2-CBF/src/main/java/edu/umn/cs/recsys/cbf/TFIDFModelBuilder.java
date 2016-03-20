fd4377b1b6581528acf31a3b946f686e4eb6d096
tardnoggle <cgault@gmail.com>
2016-03-20 18:24:38 -0500

    Doing work on the TFIDFModelBuilder class

--- Assignment-2-CBF/src/main/java/edu/umn/cs/recsys/cbf/TFIDFModelBuilder.java
+++ Assignment-2-CBF/src/main/java/edu/umn/cs/recsys/cbf/TFIDFModelBuilder.java
@@ -15,8 +15,11 @@ import org.slf4j.LoggerFactory;
 import javax.inject.Inject;
 import javax.inject.Provider;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
+import static org.lenskit.util.math.Vectors.euclideanNorm;
 
 /**
  * Builder for computing {@linkplain TFIDFModel TF-IDF models} from item tag data.  Each item is
@@ -53,10 +56,9 @@ public class TFIDFModelBuilder implements Provider<TFIDFModel> {
         // Build a map of tags to numeric IDs.  This lets you convert tags (which are strings)
         // into long IDs that you can use as keys in a tag vector.
 
-
-
         Map<String, Long> tagIds = buildTagIdMap();
         logger.info("Building model for {} tags", tagIds.size());
+
 
         // Create a map to accumulate document frequencies for the IDF computation
         Long2DoubleMap docFreq = new Long2DoubleOpenHashMap();
@@ -76,8 +78,25 @@ public class TFIDFModelBuilder implements Provider<TFIDFModel> {
             Long2DoubleMap work = new Long2DoubleOpenHashMap();
 
             // TODO Populate the work vector with the number of times each tag is applied to this item.
+            work.clear();
+            // work vector is empty
+
+            for (String tag : itemTagDAO.getItemTags(item)){
+                long tagID = tagIds.get(tag);
+
+                if(!work.containsKey(tagID))
+                    work.put(tagID, 0);
+
+                work.put(tagID,1);
+            }
 
             // TODO Increment the document frequency vector once for each unique tag on the item.
+
+            Set<String> itemTags = new HashSet<String>(itemTagDAO.getItemTags(item));
+            for (String tag : itemTags){
+                long tagID = tagIds.get(tag);
+                docFreq.put(tagID, 1);
+            }
 
             // Save the vector in our map, we'll add IDF and normalize later.
             itemVectors.put(item, work);
@@ -97,9 +116,14 @@ public class TFIDFModelBuilder implements Provider<TFIDFModel> {
         Map<Long,Long2DoubleMap> modelData = new HashMap<>();
         for (Map.Entry<Long,Long2DoubleMap> entry: itemVectors.entrySet()) {
             Long2DoubleMap tv = new Long2DoubleOpenHashMap(entry.getValue());
+
             // TODO Convert this map (vector) to a TF-IDF vector
+
+
             // TODO Normalize the TF-IDF vector to be a unit vector
             // HINT Take a look at the Vectors class in org.lenskit.util.math.
+
+            double eNorm = euclideanNorm(tv);
 
             // Store a packed version of the vector in the model data.
             modelData.put(entry.getKey(), LongUtils.frozenMap(tv));
